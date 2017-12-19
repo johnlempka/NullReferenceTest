560f8e359eb90a7c00311fec88186bd909f5385f
John Lempka <johnlempka@gmail.com>
2017-12-19 12:44:30 -0500

    Initial Commit

---
+++ NullReferenceTest/NullReferenceTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Configuration;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceStack.OrmLite;
+
+namespace NullReferenceTest
+{
+    public class Foo
+    {
+        public string Name { get; set; }
+
+        public string Key { get; set; }
+    }
+
+
+    [TestClass]
+    public class NullReferenceTest
+    {
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var dbFactory =
+                new OrmLiteConnectionFactory(ConfigurationManager.ConnectionStrings["Default"].ConnectionString, MySqlDialect.Provider);
+            Db = dbFactory.Open();
+        }
+
+        public IDbConnection Db { get; set; }
+
+        [TestMethod]
+        public void TestNullReference()
+        {
+            Db.CreateTableIfNotExists(typeof(Foo));
+
+            Db.AlterColumn(typeof(Foo), new FieldDefinition()
+            {
+                Name = "Name",
+                FieldType = typeof(string),
+                IsNullable = true,
+                DefaultValue = null
+            });
+        }
+    }
+}
---
+++ NullReferenceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NullReferenceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NullReferenceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07b8cd55-4ada-4704-9889-d791f64469a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
